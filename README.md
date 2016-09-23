5c2e871f95ba7eac5e8c8fa23d366827df3f5b87
Vasiliy Solovey <solovey@cinegy.com>
2016-09-23 17:15:30 +0300

    Added GoogleSignInComponent for rendering button and firing event on signing in success\failure

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
 # angular-google-signin
-Google Sign-In component for Angular written in TypeScript
+
+Google Sign-In component for Angular written in TypeScript.
+
+## Usage
+
+Add this script tag below in the head tag of ```index.html```:
+
+```html
+<script defer src="https://apis.google.com/js/platform.js"></script>
+```
+
+Import and add ```GoogleSignInComponent``` from this package to your Angular module:
+
+```typescript
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppComponent} from './app.component';
+import {GoogleSignInComponent} from 'angular-google-signin';
+
+@NgModule({
+  imports: [
+    BrowserModule
+  ],
+  declarations: [
+    AppComponent,
+    GoogleSignInComponent
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
+```
+
+Example of a component that is using this package:
+
+```typescript
+import {Component} from '@angular/core';
+import {GoogleSignInSuccess} from 'angular-google-signin';
+
+import GoogleUser = gapi.auth2.GoogleUser;
+import BasicProfile = gapi.auth2.BasicProfile;
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app.component.html'
+})
+export class AppComponent {
+  constructor() {
+  }
+
+  private myClientId: string = 'your-client-id-here.apps.googleusercontent.com';
+
+  onGoogleSignInSuccess(event: GoogleSignInSuccess) {
+    let googleUser: GoogleUser = event.googleUser;
+    let id: string = googleUser.getId();
+    let profile: BasicProfile = googleUser.getBasicProfile();
+    console.log('ID: ' +
+      profile
+        .getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+  }
+}
+```
+
+In a component template, put `<google-signin>` with attributes of render options and init params.
+`clientId` attribute is **required**. You don't need to write `google-signin-client_id` meta tag.
+
+```html
+<google-signin
+  [clientId]="myClientId"
+  [width]="myWidth"
+  [theme]="myTheme"
+  [scope]="myScope"
+  [longTitle]="myLongTitle"
+  (googleSignInSuccess)="onGoogleSignInSuccess($event)">
+</google-signin>
+```
+
+For more information about Google Sign-In JavaScript client, See [https://developers.google.com/identity/sign-in/web/sign-in](https://developers.google.com/identity/sign-in/web/sign-in)
+
+## Features and bugs
+
+Please file feature requests and bugs at the [issue tracker][tracker].
+
+[tracker]: https://github.com/ntaoo/ng2_g_signin/issues
+
+## Notes
+
+This package is a modification and rewriting of original work by @ntaoo [https://github.com/ntaoo/ng2_g_signin/](https://github.com/ntaoo/ng2_g_signin).
+
+Thanks @ntaoo for an idea and a component design 👍
---
+++ angular-google-signin.ts
@@ -0,0 +1,122 @@
+import {Component, AfterViewInit, ChangeDetectionStrategy, Input, Output, EventEmitter} from '@angular/core';
+
+import GoogleUser = gapi.auth2.GoogleUser;
+
+export class GoogleSignInSuccess {
+  public googleUser: GoogleUser;
+
+  constructor(googleUser: GoogleUser) {
+    this.googleUser = googleUser;
+  }
+}
+
+export class GoogleSignInFailure {
+}
+
+@Component({
+  selector: 'google-signin',
+  template: '<div [id]="id"></div>',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class GoogleSignInComponent implements AfterViewInit {
+  private id: string = 'google-signin2';
+
+  // Render options
+  @Input() private scope: string;
+
+  private _width: number;
+
+  get width(): string {
+    return this._width.toString();
+  }
+
+  @Input() set width(value: string) {
+    this._width = Number(value);
+  }
+
+  private _height: number;
+
+  get height(): string {
+    return this._height.toString();
+  }
+
+  @Input() set height(value: string) {
+    this._height = Number(value);
+    gapi.load('', '');
+  }
+
+  private _longTitle: boolean;
+
+  get longTitle(): string {
+    return this._longTitle.toString();
+  }
+
+  @Input() set longTitle(value: string) {
+    this._longTitle = Boolean(value);
+  }
+
+  @Input() private theme: string;
+
+  // Init params
+  @Input() private clientId: string;
+  @Input() private cookiePolicy: string;
+
+  private _fetchBasicProfile: boolean;
+
+  get fetchBasicProfile(): string {
+    return this._fetchBasicProfile.toString();
+  }
+
+  @Input() set fetchBasicProfile(s: string) {
+    this._fetchBasicProfile = Boolean(s);
+  }
+
+  @Input() private hostedDomain: string;
+  @Input() private openidRealm: string;
+
+  @Output() googleSignInSuccess: EventEmitter<GoogleSignInSuccess> = new EventEmitter<GoogleSignInSuccess>();
+
+  @Output() googleSignInFailure: EventEmitter<GoogleSignInFailure> = new EventEmitter<GoogleSignInFailure>();
+
+  ngAfterViewInit() {
+    this.auth2Init();
+    this.renderButton();
+  }
+
+  private auth2Init() {
+    if (this.clientId == null)
+      throw new Error(
+        'clientId property is necessary. (<google-signin clientId="..."></google-signin>)');
+
+    gapi.load('auth2', () => {
+      gapi.auth2.init({
+        client_id: this.clientId,
+        cookie_policy: this.cookiePolicy,
+        fetch_basic_profile: this._fetchBasicProfile,
+        hosted_domain: this.hostedDomain,
+        openid_realm: this.openidRealm
+      });
+    });
+  }
+
+  private handleFailure() {
+    this.googleSignInFailure.next(new GoogleSignInFailure());
+  }
+
+  private handleSuccess(googleUser: GoogleUser) {
+    this.googleSignInSuccess.next(new GoogleSignInSuccess(googleUser));
+  }
+
+  private renderButton() {
+    gapi.signin2.render(
+      this.id, {
+        scope: this.scope,
+        width: this._width,
+        height: this._height,
+        longtitle: this._longTitle,
+        theme: this.theme,
+        onsuccess: (googleUser: GoogleUser) => this.handleSuccess(googleUser),
+        onfailure: () => this.handleFailure()
+      });
+  }
+}
