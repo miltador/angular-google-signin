3b006e1c08857037c3ff0106b4bdc8e0ffca33d2
Vasiliy Solovey <solovey@cinegy.com>
2017-12-26 11:31:26 +0200

    Add usage example based on angular-cli project

---
+++ examples/angular-cli-usage/src/app/app.component.ts
@@ -0,0 +1,53 @@
+import {Component} from '@angular/core';
+import {GoogleSignInComponent, GoogleSignInSuccess} from '../../../../src/google-signin.component';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <!--The content below is only a placeholder and can be replaced.-->
+    <div style="text-align:center">
+      <h1>
+        Welcome to {{title}}!
+      </h1>
+      <google-signin
+        [clientId]="'166613181358-c44bp1j0k1sb973kni4u7qfb3ui6kus4.apps.googleusercontent.com'"
+        [width]="160"
+        [theme]="'dark'"
+        [scope]="'profile email'"
+        [longTitle]="true"
+        (googleSignInSuccess)="onGoogleSignInSuccess($event)">
+      </google-signin>
+
+      <img width="300"
+           src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
+    </div>
+    <h2>Here are some links to help you start: </h2>
+    <ul>
+      <li>
+        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
+      </li>
+      <li>
+        <h2><a target="_blank" rel="noopener" href="https://github.com/angular/angular-cli/wiki">CLI Documentation</a>
+        </h2>
+      </li>
+      <li>
+        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
+      </li>
+    </ul>
+
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'app';
+
+  public onGoogleSignInSuccess(event: GoogleSignInSuccess) {
+    let googleUser: gapi.auth2.GoogleUser = event.googleUser;
+    let id: string = googleUser.getId();
+    let profile: gapi.auth2.BasicProfile = googleUser.getBasicProfile();
+    console.log('ID: ' +
+      profile
+        .getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+  }
+}
